0a589efa87c3919027b1ec91cb0806c4842380b6
rameshbissu <rameshbissu646@gmail.com>
2024-06-30 07:37:53 +0530

    Adding 02-backroad-project-in-react folder directly from my github profile so please visit my github profile and also check that repositary if any doubt please let me know : Adding reason is to collect all the folder on one place

---
+++ 02-backroads-app-in-react/src/App.js
@@ -0,0 +1,21 @@
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <About></About>
+      <Services></Services>
+      <Tours></Tours>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
---
+++ 02-backroads-app-in-react/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "../images/logo.svg";
+import { socialLinks } from "../data";
+import PageLinks from "./PageLinks";
+import SocialLinks from "./SocialLinks";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+
+        <PageLinks parentClass="nav-links" itemClass="nav-link" />
+
+        <ul className="nav-icons">
+          {socialLinks.map((link) => {
+            return <SocialLinks key={link.id} {...link} itemClass="nav-icon" />;
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
