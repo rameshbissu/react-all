5283f66c7e705ee7124449e72bf66bcd5e45c05c
rameshbissu <rameshbissu646@gmail.com>
2024-07-12 15:21:44 +0530

    Error Handling

--- 06-react-query/starter/src/Items.jsx
+++ 06-react-query/starter/src/Items.jsx
@@ -2,15 +2,22 @@ import SingleItem from "./SingleItem";
 import { useQuery } from "@tanstack/react-query";
 import customFetch from "./utils";
 const Items = () => {
-  const { isLoading, data } = useQuery({
+  const { isLoading, data, error, isError } = useQuery({
     queryKey: ["tasks"],
     queryFn: async () => {
-      const { data } = await customFetch.get("/");
+      const { data } = await customFetch.get("/something");
       return data;
     },
   });
   if (isLoading) {
     return <p style={{ marginTop: "1rem" }}>Loading...</p>;
+  }
+  // if (isError) {
+  //   return <p style={{ marginTop: "1rem" }}>There was an error</p>;
+  // }
+  console.log(error);
+  if (error) {
+    return <p style={{ marginTop: "1rem" }}>{error.response.data}</p>;
   }
   return (
     <div className="items">
